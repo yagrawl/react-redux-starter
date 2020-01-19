c6734848cc2e2c58f971a8305638dda38972e7a4
Yash Agrawal <yagrawl2@gmail.com>
2020-01-19 11:17:20 -0800

    setup react routes

---
+++ src/pages/auth.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Auth() {
+  return (
+    <div>
+      <p>Auth</p>
+    </div>
+  );
+}
+
+export default Auth;
---
+++ src/routes.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router,
+         Switch,
+         Route } from 'react-router-dom';
+
+import Home from './pages/home';
+import Auth from './pages/auth';
+import Profile from './pages/profile';
+
+class Routes extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/auth" component={Auth} />
+          <Route path="/profile" component={Profile} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default Routes;
